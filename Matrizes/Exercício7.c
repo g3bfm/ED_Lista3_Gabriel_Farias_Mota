c2997346d6ff152551fae5fbc8380a3f5d177c08
Gabriel Mota <gabriel.mota70@outlook.com>
2026-04-13 19:53:27 -0400

    Create Exercício7.c

---
+++ Matrizes/Exercício7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+	
+    int i, j;
+    int a[10][10];
+
+    for (i = 0; i < 10; i++){
+        for (j = 0; j < 10; j++){
+
+            if (i < j){
+                a[i][j] = 2*i + 7*j - 2;
+            }
+            else if (i == j){
+                a[i][j] = 3*i*i - 1;
+            }
+            else{
+                a[i][j] = 4*i*i*i - 5*j*j + 1;
+            }
+        }
+    }
+
+    for (i = 0; i < 10; i++){
+        for (j = 0; j < 10; j++){
+            printf("%d\t", a[i][j]);
+        }
+        
+        printf("\n");
+        
+    }
+
+    return 0;
+    
+}
