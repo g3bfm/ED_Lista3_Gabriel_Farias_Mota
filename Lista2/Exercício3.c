0e6a29be9a8de514e3b100b81664efacefc6cdd9
Gabriel Mota <gabriel.mota70@outlook.com>
2026-04-13 18:11:25 -0400

    Update Exercício3.c

--- Lista2/Exercício3.c
+++ Lista2/Exercício3.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main() {
+    
     float A[10], B[10];
-
     
     printf("Digite 10 numeros reais: ");
     for(int i = 0; i < 10; i++){
         scanf("%f", &A[i]);
     }
-
     
     for(int i = 0; i < 10; i++){
         B[i] = A[i] * A[i];
     }
-
     
     printf("\nVetor original: ");
     for(int i = 0; i < 10; i++){
