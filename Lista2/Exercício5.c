286812b9b95aa7213c3ba7b238969d950eb62c7d
Gabriel Mota <gabriel.mota70@outlook.com>
2026-04-13 18:11:57 -0400

    Update Exercício5.c

--- Lista2/Exercício5.c
+++ Lista2/Exercício5.c
@@ -5,19 +5,16 @@ int main(){
     int A[10];
     int contador = 0;
 
-
     printf("Digite 10 valores inteiros: ");
     for(int i = 0; i < 10; i++) {
         scanf("%d", &A[i]);
     }
-
     
     for(int i = 0; i < 10; i++) {
         if(A[i] % 2 == 0) {
             contador++;
         }
     }
-
     
     printf("Quantidade de valores pares: %d", contador);
 
